3dd21705b31d72e38f5407f9568241d406596036
Erin Stalzer <erinstalzer@gmail.com>
2019-05-24 15:58:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,11 +54,11 @@ class Student
    end
 
   def self.new_from_db(row)
-    new_song = self.new  # self.new is the same as running Song.new
-    new_song.id = row[0]
-    new_song.name =  row[1]
-    new_song.grade = row[2]
-    new_song  # return the newly created instance
+    new_student = self.new  # self.new is the same as running student.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student  # return the newly created instance
   end
 
 end
