78281a6aed76d32fd140e8fe70a19268a7f9b4ff
Erin Stalzer <erinstalzer@gmail.com>
2019-05-24 16:01:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,4 +61,19 @@ class Student
     new_student  # return the newly created instance
   end
 
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
